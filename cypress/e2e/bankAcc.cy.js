0397f254c44d6017c66d318a288b7ec47e96e092
MilosRados <mickeyrados1@gmail.com>
2022-12-14 15:54:35 +0100

    Login Test

--- cypress/e2e/bankAcc.cy.js
+++ cypress/e2e/bankAcc.cy.js
@@ -5,6 +5,8 @@ import { RegPage } from "./pages/reg_page"
 
 const regPage = new RegPage()
 
+describe('Testing Bank Account', () =>{
+
 it('BankAccount Test', () => {
 
 cy.visit('https://parabank.parasoft.com/parabank/index.htm')
@@ -13,6 +15,25 @@ regPage.registerDataNameLname()
 regPage.registerPlaceOfLiving()
 regPage.registerPhoneSsn()
 regPage.registerUserPassConf()
+
+})
+
+it('Customer Login', () => {
+    cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+
+cy.get('input[name="username"]').type('MickoRourke').should('have.value','MickoRourke')
+
+cy.get('input[name="password"]').type('$ifra14').should('have.value','$ifra14')
+
+cy.get('input[value="Log In"]').click().should('be.enabled')
+
+cy.url().should('eq','https://parabank.parasoft.com/parabank/overview.htm')
+
+cy.contains('Accounts Overview').should('be.visible')
+
+
+
+})
 
 
 })
